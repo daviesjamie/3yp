bd65d166ee0d445362184adf932088df074017b5
Jamie Davies <jamie@jamiedavies.me>
2013-11-07 13:21:44 +0000

    Created a script to stream in english tweets.

---
+++ twitter/stream_en.py
@@ -0,0 +1,16 @@
+from twython import TwythonStreamer
+import csv
+import sys
+
+APP_KEY = 'R1zJdC7s4X4zWl9cyBtTuA'
+APP_SECRET = 'Wq7AReVM5gGVG60V3t2hrdxr3BIhPlZgWN92o7ZtsmM'
+ACCESS_TOKEN = '34354896-F96Omh8C9YX9wyKfFF9CfdP1tTSOJ09pezwkHIoGp'
+ACCESS_TOKEN_SECRET = 'DKw7igfi0ncKDK6oIcCykrazBcwsZOAwzG8BKaKkrsq60'
+
+class MyStreamer(TwythonStreamer):
+   def on_success(self, data):
+       if 'text' in data and data['lang'] == 'en':
+           print data['text']
+
+stream = MyStreamer(APP_KEY, APP_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+stream.statuses.sample()
