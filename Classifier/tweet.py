dcd5a03a45a1547acb90e6eb6eabc3e3b1154ccf
Jamie Davies <jamie@jamiedavies.me>
2014-02-16 20:38:02 +0000

    Made tweet.py runnable from command-line

--- Classifier/tweet.py
+++ Classifier/tweet.py
@@ -1,12 +1,17 @@
 __author__ = 'Jamie Davies'
 
+import auth
 import sys
 
 
 def tweet(api, status):
     """Uses an authenticated Tweepy API object to post a tweet."""
 
-    if(len(status) <= 140):
+    if len(status) <= 140 :
         api.update_status(status)
     else:
         sys.stderr.write("Tweet is too long!\n")
+
+
+if __name__ == '__main__':
+    tweet(auth.get_api(), sys.argv[1])
