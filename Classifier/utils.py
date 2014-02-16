8b3266bc2aab91b402e851ca8c859da0849568fb
Jamie Davies <jamie@jamiedavies.me>
2014-02-16 23:06:38 +0000

    Refactored credential-loading code.

---
+++ Classifier/utils.py
@@ -0,0 +1,21 @@
+__author__ = 'Jamie Davies'
+
+import json
+
+
+def _load_credentials(cred_file):
+    with open(cred_file) as f:
+        creds = json.load(f)
+    return creds
+
+
+def credentials(cred_file):
+    """Fetches OAuth credentials from a JSON file."""
+
+    creds = _load_credentials(cred_file)
+    app_key = creds['APP_KEY']
+    app_secret = creds['APP_SECRET']
+    access_token = creds['ACCESS_TOKEN']
+    access_token_secret = creds['ACCESS_TOKEN_SECRET']
+
+    return app_key, app_secret, access_token, access_token_secret
