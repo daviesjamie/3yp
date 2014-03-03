0d8b8382fdd6dd42e1400dd2d570b04c556de50b
Jamie Davies <jamie@jamiedavies.me>
2014-03-03 16:55:01 +0000

    Renamed mappings.py to fit in with naming scheme.

--- Backend/functions.py
+++ Backend/functions.py
@@ -2,7 +2,7 @@ from structures import Function
 from twokenize import tokenize
 
 
-class TokenizeTweetMapping(Function):
+class TokenizeTweetFunction(Function):
     """
     Takes in a tweet JSON object and tokenizes the tweet text using Brendan O'Connor's
     "twokenizer".
