2e739339f3eb06fbac4a26840b5254ab2aa51a33
Jamie Davies <jamie@jamiedavies.me>
2014-03-03 19:38:23 +0000

    Added English tweet Predicate, and docstrings.

--- Backend/predicates.py
+++ Backend/predicates.py
@@ -1,6 +1,25 @@
 from structures import Predicate
 
 
+class TruePredicate(Predicate):
+    """
+    Simple predicate that always returns True. Used for testing purposes only.
+    """
+    def test(self, obj):
+        return True
+
+
 class TweetsWithHashtagsPredicate(Predicate):
+    """
+    Filters tweets to only include those that contain hashtags.
+    """
     def test(self, obj):
         return len(obj['entities']['hashtags']) > 0
+
+
+class TweetsInEnglishPredicate(Predicate):
+    """
+    Filters tweets to only include those that are tagged as being in English.
+    """
+    def test(self, obj):
+        return obj['lang'] == 'en'
