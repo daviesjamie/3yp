c0622417fccd7006b2ec6a58aaa30a76e42bc115
Jamie Davies <jamie@jamiedavies.me>
2014-03-04 13:33:18 +0000

    Consolidated input streams into one file.

--- Backend/input.py
+++ Backend/input.py
@@ -1,7 +1,8 @@
+import json
 from multiprocessing import Process
 import sys
 from twython import TwythonStreamer
-from stream import BufferedStream
+from stream import BufferedStream, AbstractStream
 
 
 class TweetStream(BufferedStream):
@@ -34,3 +35,33 @@ class TweetStream(BufferedStream):
 
         def on_error(self, status_code, data):
             sys.stderr.write(status_code, data)
+
+
+class JSONInputStream(AbstractStream):
+    """
+    Loads a file containing a separate JSON object on each line into a stream for processing.
+    """
+
+    def __init__(self, filename):
+        self.input = open(filename, 'r')
+        self.next_line = None
+
+    def has_next(self):
+        if self.next_line is not None:
+            return True
+
+        try:
+            self.next_line = self.input.next()
+        except StopIteration:
+            self.next_line = None
+
+        return self.next_line is not None
+
+    def next(self):
+        if not self.has_next():
+            raise Exception("Iteration has no more elements")
+
+        to_return = json.loads(self.next_line)
+        self.next_line = None
+
+        return to_return
