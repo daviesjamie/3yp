c82e038f0fe703b546b47bb7b4b6db3d01521f72
Jamie Davies <jamie@jamiedavies.me>
2014-03-04 14:58:48 +0000

    Added operation to output tweets to JSON file.

---
+++ Backend/output.py
@@ -0,0 +1,14 @@
+import json
+import io
+from structures import Operation
+
+
+class JSONOutputOperation(Operation):
+    """
+    An operation that outputs a each object as a JSON object on a new line in the specified file.
+    """
+    def __init__(self, filename):
+        self.output = io.open(filename, 'w', encoding='utf-8')
+
+    def perform(self, obj):
+        self.output.write(unicode(json.dumps(obj, ensure_ascii=False) + "\n"))
