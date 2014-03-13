3d63b537d453e52d23a954d01680e42feb094fd1
Jamie Davies <jamie@jamiedavies.me>
2014-03-13 21:01:41 +0000

    Created script to run flask inside tornado.

---
+++ Backend/server.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from tornado.wsgi import WSGIContainer
+from devserver import app
+
+
+http_server = HTTPServer(WSGIContainer(app))
+http_server.listen(5000)
+IOLoop.instance().start()
