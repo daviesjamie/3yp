45c864cf4352c3668c667995a380a0a8248e8db3
Jamie Davies <jamie@jamiedavies.me>
2014-03-22 14:36:27 +0000

    Added functionality to post a tweet!

--- client/clever_twit/views.py
+++ client/clever_twit/views.py
@@ -1,17 +1,32 @@
 from django.conf import settings
-from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response
-
+from django.shortcuts import render
+from twython import Twython
 
 def index(request):
     if not request.user.is_authenticated():
-        return render_to_response('cover.html')
+        return render(request, 'cover.html')
 
-    return render_to_response('index.html')
+    return render(request, 'index.html')
 
 
 def tweet(request):
     if not request.user.is_authenticated():
-        return render_to_response('cover.html')
+        return render(request, 'cover.html')
 
-    return render_to_response('tweet.html')
+    posted = False
+    errors = []
+
+    if request.method == "POST":
+        status = request.POST.get('status')
+
+        if len(status) > 140:
+            errors.append("Tweet was too long!")
+        else:
+            user = request.user.twitterprofile
+            twitter = Twython(settings.TWITTER_KEY, settings.TWITTER_SECRET, user.oauth_token, user.oauth_secret)
+
+            twitter.update_status(status=status)
+            posted = True
+
+
+    return render(request, 'tweet.html', { 'posted': posted, 'errors': errors })
