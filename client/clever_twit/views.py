25e52be226cb1a7dde86dd39f6fb6cbce54a4c7c
Jamie Davies <jamie@jamiedavies.me>
2014-04-28 00:54:00 +0100

    Added search functionality.

--- client/clever_twit/views.py
+++ client/clever_twit/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from twython import Twython
+import simplejson
+import urllib2
 
 def index(request):
     if not request.user.is_authenticated():
@@ -56,6 +58,13 @@ def results(request):
     user = request.user.twitterprofile
     twitter = Twython(settings.TWITTER_KEY, settings.TWITTER_SECRET, user.oauth_token, user.oauth_secret)
 
-    results = twitter.search(q='#'+query, count=15)
+    req = urllib2.Request('http://kanga-jagd1g11.ecs.soton.ac.uk/api/hashtag/{0}?num={1}'.format(query, 10))
+    opener = urllib2.build_opener()
+    f = opener.open(req)
+    expjson = simplejson.load(f)
+    expansions = [e[0] for e in expjson]
+    expansion = ' '.join(expansions)
 
-    return render(request, 'results.html', { 'query': query, 'results': results['statuses'] })
+    results = twitter.search(q=u'{0}'.format(expansion), count=25)
+
+    return render(request, 'results.html', { 'query': query, 'expansions': expansions, 'results': results['statuses'] })
